7c2c6787ec501bb79240bf230359b5425abcf09b
raza-abbas-23sw <raza84abbas@gmail.com>
2025-06-21 19:36:25 +0500

    create read blog page

--- client/src/components/ReadBlog.jsx
+++ client/src/components/ReadBlog.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import blogCardsData from "../data/blogs/cardsData";
+import cardsData from '../data/blogs/cardsData';
 
 const ReadBlog = ({id}) => {
+    const blog=cardsData.find((blog)=>blog.id==id)
   return (
+    <>
     <div>ReadBlog {id}</div>
+        <img src={blog.imgSrc} className='w-[500px]'/>
+        <p>{blog.title}</p>
+        {console.log(blog.title)
+        }
+
+    </>
   )
 }
 
